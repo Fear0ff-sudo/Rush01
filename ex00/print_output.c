49dae2f0f60020fea18483b131b3580b4999e7ba
Axel Manet <amanet@c1r2p6.42nice.fr>
2026-07-18 15:12:43 +0200

    finished print_output.c

---
+++ ex00/print_output.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_output.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amanet <amanet@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/18 14:20:47 by amanet            #+#    #+#             */
+/*   Updated: 2026/07/18 15:11:45 by amanet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rush.h"
+
+void	print_output(struct s_skyscrapers *s1)
+{
+	int		i;
+	int		j;
+	char	ptr;
+
+	i = 0;
+	while (i <= 3)
+	{
+		j = 0;
+		while (j <= 3)
+		{
+			ptr = s1->grid[i][j];
+			write(1, &ptr, 1);
+			if (j < 3)
+				write(1, " ", 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
+/*
+#include "stdio.h"
+
+int	main(void)
+{
+	char					str[] = "1234";
+	struct s_skyscrapers	s1;
+	int						i;
+	int						j;
+
+	i = 0;
+	while (i <= 3)
+	{
+		j = 0;
+		while (j <= 3)
+		{
+			s1.grid[i][j] = str[j];
+			j++;
+		}
+		i++;
+	}
+	print_output(&s1);
+	return (0);
+}*/
