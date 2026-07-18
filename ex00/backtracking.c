e9eb3b188470fbfbf3cfd37b48e7aa13bd0478b7
Lowan Baudart <lobaudar@c1r2p4.42nice.fr>
2026-07-18 14:26:14 +0200

    .h s

--- ex00/backtracking.c
+++ ex00/backtracking.c
@@ -1,30 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush.h                                             :+:      :+:    :+:   */
+/*   backtracking.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lobaudar <lobaudar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/18 13:59:05 by lobaudar          #+#    #+#             */
-/*   Updated: 2026/07/18 14:25:33 by lobaudar         ###   ########.fr       */
+/*   Created: 2026/07/18 14:21:38 by lobaudar          #+#    #+#             */
+/*   Updated: 2026/07/18 14:25:34 by lobaudar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef RUSH_H
-# define RUSH_H
+#include "rush.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-
-
-typedef struct s_skyscrapers
-{
-	char	top[3];
-	char	bottom[3];
-	char	left[3];
-	char	right[3];
-	char	**grid;
-}				t_skyscrapers;
-
-
-#endif
